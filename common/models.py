d3aa948c50d658d2c42d86a570a10cfb25e90fb5
alexis walker <alexiswalker@hotmail.com>
2019-04-27 15:07:56 -0300

    address in common objects

--- common/models.py
+++ common/models.py
@@ -34,7 +34,19 @@ class City(TimeStampedModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,  verbose_name='Usuario')
 
     def __str__(self):
-        return '(%s) - %s' % (self.zip_code, self.name)
+        return '(%s) %s' % (self.zip_code, self.name)
 
     class Meta:
         verbose_name_plural = 'Ciudades'
+
+class Address(TimeStampedModel):
+    street_number = models.IntegerField(verbose_name='Número')
+    route = models.CharField(max_length=128, verbose_name='Calle')
+    unit = models.CharField(max_length=32, verbose_name='Unidad')
+    city  = models.ForeignKey(City, on_delete=models.PROTECT, verbose_name='Ciudad')
+
+    def __str__(self):
+        return '%s %s - %s' % (self.route, self.street_number, self.city)
+
+    class Meta:
+        verbose_name_plural = 'Direcciones'
