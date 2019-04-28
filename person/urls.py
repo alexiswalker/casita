74196aeaf9f1758421cf4b3e752093889291df7a
alexis walker <alexiswalker@hotmail.com>
2019-04-28 14:46:24 -0300

    person views

--- person/urls.py
+++ person/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import people_list_view
+from .views import people_list_view, person_create_view, person_update_view
 
 
 urlpatterns = [
     path('', login_required(people_list_view), name='people_list_view'),
+    path('add/', login_required(person_create_view), name='person_add'),
+    path('<int:pk>/', login_required(person_update_view), name='person_update'),
 ]
