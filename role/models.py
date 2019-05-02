2d321f959cbec67bf0bf8fa207bd73e69be921f6
alexis walker <alexiswalker@hotmail.com>
2019-05-02 19:20:07 -0300

    role views

--- role/models.py
+++ role/models.py
@@ -8,7 +8,20 @@ class DebtCollector(TimeStampedModel):
     person  = models.ForeignKey(Person, on_delete=models.PROTECT, verbose_name='Persona')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,  verbose_name='Usuario')
 
+    def __str__(self):
+        return '%s %s' % (self.person.last_name, self.person.first_name)
+
+    class Meta:
+        verbose_name_plural = 'Cobradores'
+
+    
 class Member(TimeStampedModel):
     person  = models.ForeignKey(Person, on_delete=models.PROTECT, verbose_name='Persona')
     debt_collector = models.ForeignKey(DebtCollector, on_delete=models.PROTECT, verbose_name='Cobrador')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT,  verbose_name='Usuario')
+
+    def __str__(self):
+        return '%s %s' % (self.person.last_name, self.person.first_name)
+
+    class Meta:
+        verbose_name_plural = 'Socios'
